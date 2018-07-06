e76cc6e843307060c7264f359d38c2ec4840c7fb
Tim Flusk <tpflusk@gmail.com>
2018-07-06 23:31:04 +0200

    It's a project

---
+++ nodetest.js
@@ -0,0 +1,12 @@
+console.log("Hello twitter and node test");
+
+const twitterController = require('twit');
+
+const T = new Twit({
+    consumer_key: '...',
+    consumer_secret: '...',
+    access_token: '...',
+    access_token_secret: '...',
+    timeout_ms: 60 * 1000,  // optional HTTP request timeout to apply to all requests.
+    strictSSL: true,     // optional - requires SSL certificates to be valid.
+});
